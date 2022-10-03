a168d64f4db4461b014bae4c54d63804a5bb5332
Hieu Vu <hieuvd@pcugroup.com>
2022-10-04 04:35:18 +0700

    9.4.22

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,60 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+import imgs from '~/components/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+import { Wrapper as PopperWrapper } from '../Popper';
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    const [searchResult, setSearchResult] = useState([]);
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1]);
+        }, 0);
+    }, []);
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={imgs.logo} alt="tiktok" />
+                </div>
+                <Tippy
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <PopperWrapper>
+                            <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                                asdsafasdf
+                            </div>
+                        </PopperWrapper>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="search accounts and videos" spellCheck={false} />
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+                {/* <Tippy
+                    render={(attrs) => (
+                        <div className="box" tabIndex="-1" {...attrs}>
+                            My asdasd
+                        </div>
+                    )}
+                >
+                    <button>My button</button>
+                </Tippy> */}
+                <div className={cx('action')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
---
+++ src/components/assets/images/index.js
@@ -0,0 +1,4 @@
+const imgs = {
+    logo: require('~/components/assets/images/logo.svg').default,
+};
+export default imgs;
